7b674618d456b8baa843c9f3b4d9f106e1a7e4d0
Towsif Ahmed Omi <towsif031@gmail.com>
2022-03-30 23:05:12 +0600

    Home page. wip.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import Home from './pages/Home';
+
+const App = () => {
 	return (
 		<div className="App">
 			<h1>Hello World!</h1>
+			<Home />
 		</div>
 	);
-}
+};
 
 export default App;
