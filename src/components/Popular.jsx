a69cfc6e369258796d7b67ab8d6aaae98f9d7934
Towsif Ahmed Omi <towsif031@gmail.com>
2022-03-30 23:45:21 +0600

    got random recipes from api.

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+
 const Popular = () => {
+	useEffect(() => {
+		getPopular();
+	}, []);
+
+	const getPopular = async () => {
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10&tags=vegetarian`
+		);
+		const data = await api.json();
+		console.log(data);
+	};
+
 	return <div>Popular</div>;
 };
 
