c4ac02165a137a330c9fd3465effe2cc510259d0
Towsif Ahmed Omi <towsif031@gmail.com>
2022-04-06 00:04:51 +0600

    cuisine recipes.

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,10 +20,35 @@ const Cuisine = () => {
 	}, [params.type]);
 
 	return (
-		<div>
-			<h3>Cuisine</h3>
-		</div>
+		<Grid>
+			{cuisine.map((recipe) => (
+				<Card key={recipe.id}>
+					<img src={recipe.image} alt="" />
+					<h4>{recipe.title}</h4>
+				</Card>
+			))}
+		</Grid>
 	);
 };
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+	grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+	img {
+		width: 100%;
+		border-radius: 2rem;
+	}
+	a {
+		text-decoration: none;
+	}
+	h4 {
+		text-align: center;
+		padding: 1rem;
+	}
+`;
 
 export default Cuisine;
