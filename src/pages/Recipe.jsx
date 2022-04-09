921cd1c3680bbde5016a3cb097549a2f4b5b12a9
Towsif Ahmed Omi <towsif031@gmail.com>
2022-04-10 01:39:40 +0600

    active instructions or ingredients tab.

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -38,11 +38,27 @@ const Recipe = () => {
 				>
 					Ingredients
 				</Button>
-				<div>
-					<h3
-						dangerouslySetInnerHTML={{ __html: details.summary }}
-					></h3>
-				</div>
+				{activeTab === 'instructions' && (
+					<div>
+						<h3
+							dangerouslySetInnerHTML={{
+								__html: details.summary
+							}}
+						></h3>
+						<h3
+							dangerouslySetInnerHTML={{
+								__html: details.instructions
+							}}
+						></h3>
+					</div>
+				)}
+				{activeTab === 'ingredients' && (
+					<ul>
+						{details.extendedIngredients.map((ingredient) => (
+							<li key={ingredient.id}>{ingredient.original}</li>
+						))}
+					</ul>
+				)}
 			</Info>
 		</DetailWrapper>
 	);
