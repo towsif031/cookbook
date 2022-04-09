8bca28c73d4bb03a9b8470b29c3d0455f6c4dec2
Towsif Ahmed Omi <towsif031@gmail.com>
2022-04-10 00:02:52 +0600

    search recipes.

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
 const Searched = () => {
-	return <div>Searched</div>;
+	const [searchedRecipes, setSearchedRecipes] = useState([]);
+	const params = useParams();
+
+	const getSearched = async (name) => {
+		const data = await fetch(
+			`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+		);
+		const recipes = await data.json();
+		setSearchedRecipes(recipes.results);
+	};
+
+	useEffect(() => {
+		getSearched(params.search);
+	}, [params.search]);
+
+	return (
+		<Grid>
+			{searchedRecipes.map((recipe) => (
+				<Card key={recipe.id}>
+					<img src={recipe.image} alt="" />
+					<h4>{recipe.title}</h4>
+				</Card>
+			))}
+		</Grid>
+	);
 };
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+	grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+	img {
+		width: 100%;
+		border-radius: 2rem;
+	}
+	a {
+		text-decoration: none;
+	}
+	h4 {
+		text-align: center;
+		padding: 1rem;
+	}
+`;
 
 export default Searched;
