11becfd8cb6b4c21a893bd3a01c44a868ef82381
Towsif Ahmed Omi <towsif031@gmail.com>
2022-04-10 23:46:15 +0600

    added logo and home navigation.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
 import Category from './components/Category';
 import Pages from './pages/Pages';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { GiKnifeFork } from 'react-icons/gi';
 
 const App = () => {
 	return (
 		<div className="App">
 			<BrowserRouter>
+				<Nav>
+					<GiKnifeFork />
+					<Logo to={'/'}>CookBook</Logo>
+				</Nav>
 				<Search />
 				<Category />
 				<Pages />
@@ -14,5 +21,23 @@ const App = () => {
 		</div>
 	);
 };
+
+const Logo = styled(Link)`
+	text-decoration: none;
+	font-size: 1.5rem;
+	font-weight: 400;
+	font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+	padding: 4rem 0;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+
+	svg {
+		font-size: 2rem;
+	}
+`;
 
 export default App;
